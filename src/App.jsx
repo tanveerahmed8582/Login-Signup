fda040cb88bce8004992d5584a5eeac562994b09
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2025-11-05 14:48:47 +0530

    build login-signup page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+
+function App() {
+  return (
+    <>
+      <LoginSignup />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const LoginSignup = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <>
+      <div className="container">
+        <div className="form-container">
+          <div className="form-toggle">
+            <button
+              className={isLogin ? "active" : ""}
+              onClick={() => setIsLogin(true)}
+            >
+              Login
+            </button>
+            <button
+              className={!isLogin ? "active" : ""}
+              onClick={() => setIsLogin(false)}
+            >
+              SignUp
+            </button>
+          </div>
+          {isLogin ? (
+            <>
+              <div className="form">
+                <h2>Login Form</h2>
+                <input type="email" placeholder="Enter email" />
+                <input type="password" placeholder="Enter Password" />
+                <a href="#">Forget Password</a>
+                <button>Login</button>
+                <p>
+                  Not a Member?{" "}
+                  <a
+                    href="#"
+                    onClick={() => {
+                      setIsLogin(false);
+                    }}
+                  >
+                    Signup Now
+                  </a>
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="form">
+                <h2>Signup Form</h2>
+                <input type="email" placeholder="Enter Email" />
+                <input type="password" placeholder="Enter Password" />
+                <input type="password" placeholder="Confirm Password" />
+                <button>Signup</button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default LoginSignup;
